01f39ef72f7d44f979d4c7a914a2ddad15334631
Fryman69 <88807353+Fryman69@users.noreply.github.com>
2026-01-08 13:26:36 +0100

    Add files via upload

--- ukoly1.py
+++ ukoly1.py
@@ -1,11 +1,17 @@
+import csv 
+sladkost = []
+velikost = []
+prumers = 0
+prumerv = 0
+cesta = r"C:\Users\st025547\Downloads\Banana_Quality-main\Banana_Quality-main\banana_quality.csv"
 
-file_path = '2. prace_se_soubory/data/chatlog.txt'
+with open(cesta, "r", encoding="utf-8") as file:
+    for radek in csv.DictReader(file):
+        if float(radek["Weight"]) >= 1:
+            if float(radek["Sweetness"]) >= 3.5:
+                sladkost.append(float(radek["Sweetness"]))
+                velikost.append(float(radek["Weight"]))
+prumers = sum(sladkost) / len(sladkost)
+prumerv = sum(velikost) / len(velikost)
 
-nigiman = ""
-jmeno = ""
-zprava = ""
-while zprava != "konec" and zprava !="Konec":
-    jmeno = input("Zadej uzivatelske jmeno: ")
-    zprava = input("Zadej zpravu: ")
-    with open(file_path, "a", encoding="utf-8") as file:
-        file.write(f'{jmeno}: {zprava}\n')
+print("Velke banany jsou prumerne ", prumers," jednotek sladke a ", prumerv, " kilo tezke.")
